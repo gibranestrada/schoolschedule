2cdc521b9068ab8b74247057b61bf8108c6edcc3
Gibran <gibran9@gmail.com>
2024-02-19 21:08:22 -0500

    Updates

---
+++ precache-manifest.a30599b01d6e5e2101ee15828e33f669.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36c55fe77bb872b45576b04027119b94",
+    "url": "/schoolschedule/index.html"
+  },
+  {
+    "revision": "9945c9a6cab6d4af7da0",
+    "url": "/schoolschedule/static/css/main.2885014a.chunk.css"
+  },
+  {
+    "revision": "051ed29e99cebaa72346",
+    "url": "/schoolschedule/static/js/2.e13566ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/schoolschedule/static/js/2.e13566ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9945c9a6cab6d4af7da0",
+    "url": "/schoolschedule/static/js/main.74294c8b.chunk.js"
+  },
+  {
+    "revision": "d435214d978e41f7a4f6",
+    "url": "/schoolschedule/static/js/runtime-main.5daf7fc5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.95cbe0b13a9b339cf7b8dccdfeeb3afb.js"
+  "/schoolschedule/precache-manifest.a30599b01d6e5e2101ee15828e33f669.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/schoolschedule/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
