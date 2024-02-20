ac24968b004b93da1c0bd4050216684cd3e1f018
Gibran <gibran9@gmail.com>
2024-02-19 21:05:04 -0500

    Updates

---
+++ precache-manifest.95cbe0b13a9b339cf7b8dccdfeeb3afb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "158623465e7c0702b5b4b34596f6b611",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6824c9deef940fa0ee21",
+    "url": "/static/css/main.2885014a.chunk.css"
+  },
+  {
+    "revision": "051ed29e99cebaa72346",
+    "url": "/static/js/2.e13566ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e13566ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6824c9deef940fa0ee21",
+    "url": "/static/js/main.d6ec5277.chunk.js"
+  },
+  {
+    "revision": "0e9acf391dbebf57d38c",
+    "url": "/static/js/runtime-main.217873f9.js"
+  }
+]);
